5f6e6758206578158570dbd9963abf0866bcc581
Jorge Acetozi <jorge.acetozi@gmail.com>
2019-01-28 17:36:52 -0200

    Create Binary Search Tree with insert operation

---
+++ src/main/java/com/jorgeacetozi/algorithms/binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,28 @@
+package com.jorgeacetozi.algorithms.binarySearchTree;
+
+class BinarySearchTree {
+
+  Node root;
+
+  void insert(int key, String value) {
+    if (root == null) {
+      this.root = new Node(key, value);
+      return;
+    } else {
+      insertRecursive(this.root, key, value);
+    }
+  }
+
+  private Node insertRecursive(Node currentNode, int key, String value) {
+    if (currentNode == null) {
+      return new Node(key, value);
+    }
+    if (key < currentNode.key) {
+      currentNode.leftChild = insertRecursive(currentNode.leftChild, key, value);
+    } else if (key > currentNode.key) {
+      currentNode.rightChild = insertRecursive(currentNode.rightChild, key, value);
+    }
+    return currentNode;
+  }
+
+}
---
+++ src/main/java/com/jorgeacetozi/algorithms/binarySearchTree/Node.java
@@ -0,0 +1,17 @@
+package com.jorgeacetozi.algorithms.binarySearchTree;
+
+class Node {
+  int key;
+  Object value;
+  Node leftChild;
+  Node rightChild;
+  
+  Node(int key, Object value){
+    this.key = key;
+    this.value = value;
+  }
+  @Override
+  public String toString() {
+    return String.format("Key: %d | Value: %s", this.key, this.value);
+  }
+}
