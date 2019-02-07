23d21885538bbcaaec9ca68061d33ba2fc9d932f
Jorge Acetozi <jorge.acetozi@gmail.com>
2019-02-07 16:56:21 -0200

    Implemente remove operation for Heap

--- src/main/java/com/jorgeacetozi/algorithms/heap/MinHeap.java
+++ src/main/java/com/jorgeacetozi/algorithms/heap/MinHeap.java
@@ -1,5 +1,7 @@
 package com.jorgeacetozi.algorithms.heap;
 
+import java.util.Arrays;
+
 class MinHeap {
 
   int heap[];
@@ -7,22 +9,69 @@ class MinHeap {
 
   MinHeap() {
     this.heap = new int[10];
+    Arrays.fill(this.heap, -1);
     this.heapSize = 0;
   }
 
-  public void insert(int value) {
+  void insert(int value) {
     heap[heapSize++] = value;
-    fixUp(heapSize - 1);
+    heapifyUp(heapSize - 1);
   }
 
-  private void fixUp(int i) {
-    int parentIndex = (i - 1) / 2;
+  private void heapifyUp(int currentNodeIndex) {
+    if (currentNodeIndex == 0) { // we reached to the root node
+      return;
+    }
 
-    if (parentIndex >= 0 && heap[parentIndex] > heap[i]) {
+    int parentIndex = (currentNodeIndex - 1) / 2;
+
+    if (heap[parentIndex] > heap[currentNodeIndex]) {
       int temp = heap[parentIndex];
-      heap[parentIndex] = heap[i];
-      heap[i] = temp;
-      fixUp(parentIndex);
+      heap[parentIndex] = heap[currentNodeIndex];
+      heap[currentNodeIndex] = temp;
+      heapifyUp(parentIndex);
+    }
+  }
+
+  int remove() {
+    int rootElement = heap[0];
+
+    heap[0] = heap[heapSize - 1];
+    heap[heapSize - 1] = -1;
+    heapSize--;
+
+    heapifyDown(0);
+    return rootElement;
+  }
+
+  private void heapifyDown(int currentNodeIndex) {
+    int leftChildIndex = 2 * currentNodeIndex + 1;
+    int rightChildIndex = 2 * currentNodeIndex + 2;
+
+    boolean hasLeftChild = leftChildIndex < heapSize;
+    boolean hasRightChild = rightChildIndex < heapSize;
+
+    if (!hasLeftChild && !hasRightChild) { // it's a leaf, so it's a valid heap
+      return;
+    } else if (hasLeftChild && !hasRightChild) { // there is only the left child
+      if (heap[currentNodeIndex] > heap[leftChildIndex]) {
+        int temp = heap[currentNodeIndex];
+        heap[currentNodeIndex] = heap[leftChildIndex];
+        heap[leftChildIndex] = temp;
+        heapifyDown(leftChildIndex);
+      }
+    } else { // the node contains left and right child
+      if (heap[leftChildIndex] < heap[rightChildIndex]) {
+        int temp = heap[currentNodeIndex];
+        heap[currentNodeIndex] = heap[leftChildIndex];
+        heap[leftChildIndex] = temp;
+        heapifyDown(leftChildIndex);
+      } else {
+        int temp = heap[currentNodeIndex];
+        heap[currentNodeIndex] = heap[rightChildIndex];
+        heap[rightChildIndex] = temp;
+        heapifyDown(rightChildIndex);
+      }
     }
   }
 }
--- src/test/java/com/jorgeacetozi/algorithms/heap/MinHeapTest.java
+++ src/test/java/com/jorgeacetozi/algorithms/heap/MinHeapTest.java
@@ -22,4 +22,94 @@ public class MinHeapTest {
     assertThat(minHeap.heap[3], equalTo(30));
     assertThat(minHeap.heap[4], equalTo(25));
   }
+
+  @Test
+  public void shouldRemoveHeapRoot() {
+    minHeap.insert(7);
+    assertThat(minHeap.remove(), equalTo(7));
+    assertThat(minHeap.heapSize, equalTo(0));
+    assertThat(minHeap.heap[0], equalTo(-1));
+  }
+
+  @Test
+  public void shouldRemoveRootWithLeftChildOnly() {
+    minHeap.insert(7);
+    minHeap.insert(3);
+    assertThat(minHeap.remove(), equalTo(3));
+    assertThat(minHeap.heapSize, equalTo(1));
+    assertThat(minHeap.heap[0], equalTo(7));
+  }
+
+  @Test
+  public void shouldRemoveRootWithTwoChildrem() {
+    minHeap.insert(7);
+    minHeap.insert(3);
+    minHeap.insert(10);
+    assertThat(minHeap.remove(), equalTo(3));
+    assertThat(minHeap.heapSize, equalTo(2));
+    assertThat(minHeap.heap[0], equalTo(7));
+    assertThat(minHeap.heap[1], equalTo(10));
+  }
+
+  @Test
+  public void shouldRemoveWithLeftChild() {
+    minHeap.insert(7);
+    minHeap.insert(25);
+    minHeap.insert(10);
+    minHeap.insert(30);
+    minHeap.insert(40);
+    minHeap.insert(15);
+
+    assertThat(minHeap.remove(), equalTo(7));
+    assertThat(minHeap.heapSize, equalTo(5));
+    assertThat(minHeap.heap[0], equalTo(10));
+    assertThat(minHeap.heap[1], equalTo(25));
+    assertThat(minHeap.heap[2], equalTo(15));
+    assertThat(minHeap.heap[3], equalTo(30));
+    assertThat(minHeap.heap[4], equalTo(40));
+    assertThat(minHeap.heap[5], equalTo(-1));
+  }
+
+  @Test
+  public void shouldRemoveWithLeftAndRightChildrem() {
+    minHeap.insert(7);
+    minHeap.insert(25);
+    minHeap.insert(10);
+    minHeap.insert(30);
+    minHeap.insert(40);
+    minHeap.insert(15);
+    minHeap.insert(12);
+
+    assertThat(minHeap.remove(), equalTo(7));
+    assertThat(minHeap.heapSize, equalTo(6));
+    assertThat(minHeap.heap[0], equalTo(10));
+    assertThat(minHeap.heap[1], equalTo(25));
+    assertThat(minHeap.heap[2], equalTo(12));
+    assertThat(minHeap.heap[3], equalTo(30));
+    assertThat(minHeap.heap[4], equalTo(40));
+    assertThat(minHeap.heap[5], equalTo(15));
+    assertThat(minHeap.heap[6], equalTo(-1));
+  }
+
+  @Test
+  public void shouldRemoveWithManyFixDownOperations() {
+    minHeap.insert(7);
+    minHeap.insert(25);
+    minHeap.insert(10);
+    minHeap.insert(30);
+    minHeap.insert(40);
+    minHeap.insert(15);
+    minHeap.insert(12);
+    minHeap.insert(50);
+
+    assertThat(minHeap.remove(), equalTo(7));
+    assertThat(minHeap.heapSize, equalTo(7));
+    assertThat(minHeap.heap[0], equalTo(10));
+    assertThat(minHeap.heap[1], equalTo(25));
+    assertThat(minHeap.heap[2], equalTo(12));
+    assertThat(minHeap.heap[3], equalTo(30));
+    assertThat(minHeap.heap[4], equalTo(40));
+    assertThat(minHeap.heap[5], equalTo(15));
+    assertThat(minHeap.heap[6], equalTo(50));
+  }
 }
