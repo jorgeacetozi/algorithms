8c0f7fb81e74f9509cd797d5ec4110beb22a80ed
Jorge Acetozi <jorge.acetozi@gmail.com>
2019-02-15 07:10:23 -0200

    Implement Least Recently Used (LRU) cache

---
+++ src/main/java/com/jorgeacetozi/algorithms/cache/leastRecentlyUsed/LRUCache.java
@@ -0,0 +1,70 @@
+package com.jorgeacetozi.algorithms.cache.leastRecentlyUsed;
+
+import java.util.Optional;
+import com.jorgeacetozi.algorithms.cache.leastRecentlyUsed.doublyLinkedList.DoublyLinkedList;
+import com.jorgeacetozi.algorithms.cache.leastRecentlyUsed.doublyLinkedList.Node;
+import com.jorgeacetozi.algorithms.cache.leastRecentlyUsed.hashTable.HashTable;
+
+class LRUCache<K, V> {
+
+  private HashTable<K, Node<V>> hashTable;
+  private DoublyLinkedList<V> doublyLinkedList;
+  private int capacity;
+
+  public LRUCache(int capacity) {
+    hashTable = new HashTable<>(capacity);
+    doublyLinkedList = new DoublyLinkedList<>();
+    this.capacity = capacity;
+  }
+
+  public Optional<V> get(K key) {
+    Optional<Node<V>> optionalNode = hashTable.get(key);
+
+    if (optionalNode.isPresent()) {
+      Node<V> node = optionalNode.get();
+      updateRecentlyUsed(node);
+      return Optional.of(node.getValue());
+    } else {
+      return Optional.empty();
+    }
+  }
+
+  public void put(K key, V value) {
+    Optional<Node<V>> nodeOptional = hashTable.get(key);
+
+    if (!nodeOptional.isPresent()) {
+      if (isCacheFull()) {
+        evict(key);
+      }
+      insert(key, value);
+    } else {
+      Node<V> node = nodeOptional.get();
+      node.setValue(value);
+      updateRecentlyUsed(node);
+    }
+  }
+
+  public void print() {
+    doublyLinkedList.print();
+  }
+
+  private void evict(K key) {
+    doublyLinkedList.removeEnd();
+    hashTable.remove(key);
+  }
+
+  private void insert(K key, V value) {
+    Node<V> newNode = new Node<>(value);
+    doublyLinkedList.insertStart(newNode);
+    hashTable.put(key, newNode);
+  }
+
+  private void updateRecentlyUsed(Node<V> node) {
+    doublyLinkedList.remove(node);
+    doublyLinkedList.insertStart(node);
+  }
+
+  private boolean isCacheFull() {
+    return this.capacity == hashTable.getSize();
+  }
+}
