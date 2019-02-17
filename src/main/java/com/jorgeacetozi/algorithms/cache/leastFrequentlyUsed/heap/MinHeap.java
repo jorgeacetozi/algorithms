3482b6beb3396a5917129e39934a6dbebc2f0a89
Jorge Acetozi <jorge.acetozi@gmail.com>
2019-02-17 12:03:43 -0300

    Implement Min Heap for storing cache items in a LFU fashion

---
+++ src/main/java/com/jorgeacetozi/algorithms/cache/leastFrequentlyUsed/heap/MinHeap.java
@@ -0,0 +1,92 @@
+package com.jorgeacetozi.algorithms.cache.leastFrequentlyUsed.heap;
+
+import java.util.Stack;
+
+public class MinHeap<K, V> {
+
+  private Node<K, V>[] heap;
+  private int size;
+
+  @SuppressWarnings("unchecked")
+  public MinHeap(int capacity) {
+    heap = new Node[capacity];
+  }
+
+  public void insert(Node<K, V> newNode) {
+    heap[size++] = newNode;
+    heapifyUp(size - 1);
+  }
+
+  private void heapifyUp(int currentNodeIndex) {
+    if (currentNodeIndex != 0) { // we haven't reached to the root
+      int parentIndex = (currentNodeIndex - 1) / 2;
+
+      if (heap[parentIndex].compareTo(heap[currentNodeIndex]) > 0) {
+        Node<K, V> temp = heap[parentIndex];
+        heap[parentIndex] = heap[currentNodeIndex];
+        heap[currentNodeIndex] = temp;
+        heapifyUp(parentIndex);
+      }
+    }
+  }
+
+  public void updateFrequency(Node<K, V> node) {
+    node.setFrequency(node.getFrequency() + 1);
+    for (int i = 0; i < size; i++) {
+      if (heap[i] == node) {
+        heapifyDown(i);
+        break;
+      }
+    }
+  }
+
+  public Node<K, V> remove() {
+    Node<K, V> leastFrequentlyUsed = heap[0];
+    heap[0] = heap[--size];
+    heapifyDown(0);
+    return leastFrequentlyUsed;
+  }
+
+  private void heapifyDown(int currentNodeIndex) {
+    int leftChildIndex = 2 * currentNodeIndex + 1;
+    int rightChildIndex = 2 * currentNodeIndex + 2;
+
+    boolean hasLeftChild = leftChildIndex < size;
+    boolean hasRightChild = rightChildIndex < size;
+
+    if (!hasLeftChild && !hasRightChild) {
+      return;
+    }
+    if (hasLeftChild && !hasRightChild) {
+      if (heap[currentNodeIndex].compareTo(heap[leftChildIndex]) > 0) {
+        swap(currentNodeIndex, leftChildIndex);
+      }
+    } else {
+      if (heap[leftChildIndex].compareTo(heap[rightChildIndex]) < 0) {
+        if (heap[currentNodeIndex].compareTo(heap[leftChildIndex]) > 0) {
+          swap(currentNodeIndex, leftChildIndex);
+        }
+      } else {
+        if (heap[currentNodeIndex].compareTo(heap[rightChildIndex]) > 0) {
+          swap(currentNodeIndex, rightChildIndex);
+        }
+      }
+    }
+  }
+
+  private void swap(int currentNodeIndex, int leftChildIndex) {
+    Node<K, V> temp = heap[leftChildIndex];
+    heap[leftChildIndex] = heap[currentNodeIndex];
+    heap[currentNodeIndex] = temp;
+    heapifyDown(leftChildIndex);
+  }
+
+  public void print() {
+    for (int i = 0; i < heap.length; i++) {
+      if (heap[i] != null) {
+        System.out.print(heap[i] + " | ");
+      }
+    }
+    System.out.println();
+  }
+}
---
+++ src/main/java/com/jorgeacetozi/algorithms/cache/leastFrequentlyUsed/heap/Node.java
@@ -0,0 +1,48 @@
+package com.jorgeacetozi.algorithms.cache.leastFrequentlyUsed.heap;
+
+public class Node<K, V> implements Comparable<Node<K, V>>{
+
+  private K key;
+  private V value;
+  private Integer frequency;
+  
+  public Node(K key, V value) {
+    this.key = key;
+    this.value = value;
+    this.frequency = 0;
+  }
+  
+  @Override
+  public int compareTo(Node<K, V> another) {
+    return this.frequency.compareTo(another.getFrequency());
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public void setKey(K key) {
+    this.key = key;
+  }
+
+  public V getValue() {
+    return value;
+  }
+
+  public void setValue(V value) {
+    this.value = value;
+  }
+
+  public Integer getFrequency() {
+    return frequency;
+  }
+
+  public void setFrequency(Integer frequency) {
+    this.frequency = frequency;
+  }
+  
+  @Override
+  public String toString() {
+    return String.format("%s %s %d", this.key, this.value, this.frequency);
+  }
+}
