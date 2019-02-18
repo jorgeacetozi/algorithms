c45ee5d5c0ade7d66947427d077b7c8973dc6125
Jorge Acetozi <jorge.acetozi@gmail.com>
2019-02-18 16:58:12 -0300

    Dynamic Array Stack tests

--- src/main/java/com/jorgeacetozi/algorithms/stack/Stack.java
+++ src/main/java/com/jorgeacetozi/algorithms/stack/Stack.java
@@ -1,9 +1,11 @@
 package com.jorgeacetozi.algorithms.stack;
 
 public interface Stack<T> {
-  
+
   void push(T item);
+
   T pop();
+
   T peek();
 
 }
---
+++ src/test/java/com/jorgeacetozi/algorithms/stack/simpleDynamicArray/SimpleDynamicArrayStackTest.java
@@ -0,0 +1,63 @@
+package com.jorgeacetozi.algorithms.stack.simpleDynamicArray;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import java.util.EmptyStackException;
+import org.junit.Test;
+
+public class SimpleDynamicArrayStackTest {
+  SimpleDynamicArrayStack<String> stack = new SimpleDynamicArrayStack<>(3);
+
+  @Test
+  public void shouldPushItemsIntoStack() {
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+    assertThat(stack.size, equalTo(3));
+  }
+
+  @Test
+  public void shouldResizeWhenStackIsFull() {
+    assertThat(stack.size, equalTo(0));
+    assertThat(stack.capacity, equalTo(3));
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+    assertThat(stack.size, equalTo(3));
+    assertThat(stack.capacity, equalTo(6));
+  }
+
+  @Test
+  public void shouldGetItemsFromStack() {
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+
+    assertThat(stack.pop(), equalTo("cobra"));
+    assertThat(stack.pop(), equalTo("xuxa"));
+    assertThat(stack.pop(), equalTo("jorge"));
+  }
+
+  @Test(expected = EmptyStackException.class)
+  public void shouldThrowExceptionWhenPoppingAnEmptyStack() {
+    stack.pop();
+  }
+
+  @Test
+  public void shouldReturnPeekItem() {
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+
+    assertThat(stack.peek(), equalTo("cobra"));
+    stack.pop();
+    assertThat(stack.peek(), equalTo("xuxa"));
+    stack.pop();
+    assertThat(stack.peek(), equalTo("jorge"));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void shouldThrowExceptionWhenReturningPeekItemFromEmptyList() {
+    stack.peek();
+  }
+}
