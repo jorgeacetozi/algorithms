cb68116539094ba0b856615936c94ab5152abffa
Jorge Acetozi <jorge.acetozi@gmail.com>
2019-02-18 12:23:49 -0300

    Stack: tests for stack implementation with simply static array

---
+++ src/test/java/com/jorgeacetozi/algorithms/stack/SimpleArrayStackTest.java
@@ -0,0 +1,60 @@
+package com.jorgeacetozi.algorithms.stack;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class SimpleArrayStackTest {
+
+  SimpleArrayStack<String> stack = new SimpleArrayStack<>(3);
+
+  @Test
+  public void shouldPushItemsIntoStack() {
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+    assertThat(stack.size, equalTo(3));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void shouldThrowExceptionWhenPushingItemsIntoFullStack() {
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+    stack.push("sorry, stack is full");
+  }
+
+  @Test
+  public void shouldGetItemsFromStack() {
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+
+    assertThat(stack.pop(), equalTo("cobra"));
+    assertThat(stack.pop(), equalTo("xuxa"));
+    assertThat(stack.pop(), equalTo("jorge"));
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void shouldThrowExceptionWhenPoppingAnEmptyStack() {
+    stack.pop();
+  }
+
+  @Test
+  public void shouldReturnPeekItem() {
+    stack.push("jorge");
+    stack.push("xuxa");
+    stack.push("cobra");
+
+    assertThat(stack.peek(), equalTo("cobra"));
+    stack.pop();
+    assertThat(stack.peek(), equalTo("xuxa"));
+    stack.pop();
+    assertThat(stack.peek(), equalTo("jorge"));
+  }
+  
+  @Test(expected = RuntimeException.class)
+  public void shouldThrowExceptionWhenReturningPeekItemFromEmptyList() {
+    stack.peek();
+  }
+}
